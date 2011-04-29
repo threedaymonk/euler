4fea98b06d32afc33db6a66354ff973308e5f9fa
Paul Battley <pbattley@gmail.com>
2011-04-29 22:34:37 +0100

    It's quicker to use built-in string functions to detect palindromes.

--- 004.hs
+++ 004.hs
@@ -13,10 +13,4 @@ main = putStrLn $ show ans
 ans = maximum [ i*j | i <- factors, j <- factors, isPalindrome(i*j) ]
   where factors = [100..999]
 
-isPalindrome n = n == reverseDigits n
-
-reverseDigits n = sum $ map reversedValueAt [1 .. (numberOfDigits)]
-  where
-    digitAt i = (n `mod` (10 ^ i)) `div` (10 ^ (i - 1))
-    reversedValueAt i = digitAt i * (10 ^ (numberOfDigits - i))
-    numberOfDigits = ceiling $ logBase 10 (fromIntegral n)
+isPalindrome n = (show n) == (reverse (show n))
