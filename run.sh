3c7b4ab39c5f61ecccca993f0efa507b60bc4cfd
Paul Battley <pbattley@gmail.com>
2012-05-06 21:40:20 +0100

    Better run script.

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+set -e
 
-target="tmp/compiled"
-mkdir -p tmp
-hlint $1 && \
-ghc $1 euler.hs --make -o $target && \
-rm -f *.hi *.o && ./$target && \
-rm -f $target
+wd=tmp
+target="$wd/$(basename $1 .hs)"
+
+mkdir -p $wd
+hlint $1
+ghc $1 euler.hs --make -o $target
+rm -f *.hi *.o
+./$target
